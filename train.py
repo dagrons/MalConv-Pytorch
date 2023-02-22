097ea067ec0423414071f799e4d2df9aba4bb725
dagrons <heyuehuii@126.com>
2023-02-22 14:51:15 +0800

    fix bugs

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import yaml
 import numpy as np
 import pandas as pd
 from src.util import ExeDataset, write_pred
-from src.model import MalConv, MalConvPadding, MalConvMaskFirstBlock
+from src.model import MalConv
 from torch.utils.data import DataLoader
 import torch
 import torch.nn as nn
